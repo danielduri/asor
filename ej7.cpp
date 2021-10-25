2037d759b38d225f29c7f1da52565f91ed88c851
Usuario VMs <danieldu@ucm.es>
2021-10-25 10:31:52 +0200

    ej7

--- ej7.cpp
+++ ej7.cpp
@@ -7,6 +7,6 @@ int main(int argc, char const *argv[])
 {
 	std::cout << "Link max: " << pathconf(path, _PC_LINK_MAX) << std::endl;
 	std::cout << "Name max: " << pathconf(path, _PC_NAME_MAX) << std::endl;
-
+	std::cout << "Path max: " << pathconf(path, _PC_PATH_MAX) << std::endl;
 	return 0;
 }
